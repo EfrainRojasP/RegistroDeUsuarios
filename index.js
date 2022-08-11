b0c796311e2bd4bb925e936bc80367ff41f0942d
EfrainRojas <efrainulisesrojaspina@gmail.com>
2022-08-11 00:10:05 -0500

    Conexion a la base de datos

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.clear();
+
+import createExpressServer from "express";
+import dotenv from "dotenv";
+import {inicarConexionMYSQL} from "./ConexionBD/ConexionMYSQL.js"
+
+dotenv.config()
+const expressApp = createExpressServer();
+const PORT = process.env.PORT;
+
+const main = async () => {  
+    try {
+        await inicarConexionMYSQL();
+        console.log("CONEXION EXITOSA A LA BD");
+    } catch (error) {
+        console.error("ERROR EN LA CONEXION A LA BD", error);
+        return;
+    }
+    expressApp.listen(PORT, (req, res) => {
+        console.log("Servidor listo en el puerto " + PORT);
+    })
+}
+main();
